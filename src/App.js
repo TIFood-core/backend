ea0ed25f051a83a05e3e543fd78f4c9e20fc3b28
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-03-24 21:20:37 -0300

    Added routes file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import express from "express";
 import cors from "cors";
 
-import { JsonSyntaxError } from "./helpers/error";
+import routes from "./routes/routes.js";
+import { JsonSyntaxError } from "./helpers/error.js";
 
 class App {
     constructor() {
         this.server = express();
         this.middlewares();
+        this.routes();
     }
 
     middlewares() {
         this.server.use(cors());
         this.server.use(express.json());
         this.server.use(JsonSyntaxError);
+    }
+
+    routes() {
+        this.server.use(routes)
     }
 }
 
