a3a7ef1a9fdccc761add9dade311094b42eb9385
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-03-31 14:17:03 -0300

    Initial server

---
+++ src/app.rs
@@ -0,0 +1,7 @@
+use axum::Router;
+
+pub async fn create_app() -> Router {
+    let app = Router::new();
+
+    app
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use app::create_app;
+use tokio::net::TcpListener;
+
+mod app;
+
+#[tokio::main]
+async fn main() {
+    let port = 3000;
+
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .expect("Failed to bind port");
+
+    let app = create_app().await;
+
+    println!("Server listening on port {}", port);
+
+    axum::serve(listener, app).await.unwrap();
 }
